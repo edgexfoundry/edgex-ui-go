452db79fde4cb0d9e9a647081498713031c806ee
yanghua <yanghua1127@gmail.com>
2018-06-24 09:53:27 +0800

    #41 Move javascript code snippets into specific javascript files
    Signed-off-by: yanghua <yanghua1127@gmail.com>

---
+++ web/static/js/index.js
@@ -0,0 +1,16 @@
+(function() {
+    var X_Session_Token = window.sessionStorage.getItem("X_Session_Token")
+    if (X_Session_Token == "" || X_Session_Token == "undefined") {
+        window.location.href = '/login.html?ran=' + Math.random();
+    }
+})()
+
+(function() {
+    window.history.pushState(null, null, "/");
+})();
+
+//Disable page forward and backward
+history.pushState(null, null, document.URL);
+window.addEventListener('popstate', function() {
+    history.pushState(null, null, document.URL);
+});
