d50e7964053fc8117e3348409635e0cc7a8b9944
yanghua <yanghua1127@gmail.com>
2018-07-01 16:19:58 +0800

    #47 Implement DAO interface with MongoDB for current domain objects
    Signed-off-by: yanghua <yanghua1127@gmail.com>

--- web/app/domain.go
+++ web/app/domain.go
@@ -17,26 +17,28 @@
 
 package app
 
+import "gopkg.in/mgo.v2/bson"
+
 type User struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+	Id       bson.ObjectId `bson:"_id,omitempty"`
+	Name     string        `json:"name"`
+	Password string        `json:"password"`
 }
 
 type Gateway struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Address     string `json:"address"`
+	Id          bson.ObjectId `bson:"_id,omitempty"`
+	Name        string        `json:"name"`
+	Description string        `json:"description"`
+	Address     string        `json:"address"`
 }
 
 type Addressable struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Protocol string `json:"protocol"`
-	Address  string `json:"address"`
-	Port     int    `json:"port"`
-	Path     string `json:"path"`
+	Id       bson.ObjectId `bson:"_id,omitempty"`
+	Name     string        `json:"name"`
+	Protocol string        `json:"protocol"`
+	Address  string        `json:"address"`
+	Port     int           `json:"port"`
+	Path     string        `json:"path"`
 
 	Publisher string `json:"publisher"`
 	User      string `json:"user"`
