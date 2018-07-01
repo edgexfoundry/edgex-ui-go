e131389be6b0251d769c010f9fe3482e597e6e38
yanghua <yanghua1127@gmail.com>
2018-07-01 16:19:58 +0800

    #47 Implement DAO interface with MongoDB for current domain objects
    Signed-off-by: yanghua <yanghua1127@gmail.com>

--- web/app/dao.go
+++ web/app/dao.go
@@ -6,29 +6,75 @@
 
 package app
 
+import (
+	"errors"
+)
+
+const (
+	UserTable        = "user"
+	GatewayTable     = "gateway"
+	AddressableTable = "addressable"
+)
+
 type UserDao interface {
-	Get(id string) (User, error)
-	GetAll() ([]User, error)
-	Exists(id string) (bool, error)
-	Add(user User) (string, error)
-	Update(user User) error
-	Remove(id string) error
+	Get(id string, dbClient DBClient) (User, error)
+	GetAll(dbClient DBClient) ([]User, error)
+	Exists(id string, dbClient DBClient) (bool, error)
+	Add(user User, dbClient DBClient) (string, error)
+	Update(user User, dbClient DBClient) error
+	Remove(id string, dbClient DBClient) error
 }
 
 type GatewayDao interface {
-	Get(id string) (Gateway, error)
-	GetAll() ([]Gateway, error)
-	Exists(id string) (bool, error)
-	Add(gateway Gateway) (string, error)
-	Update(gateway Gateway) error
-	Remove(id string) error
+	Get(id string, dbClient DBClient) (Gateway, error)
+	GetAll(dbClient DBClient) ([]Gateway, error)
+	Exists(id string, dbClient DBClient) (bool, error)
+	Add(gateway Gateway, dbClient DBClient) (string, error)
+	Update(gateway Gateway, dbClient DBClient) error
+	Remove(id string, dbClient DBClient) error
 }
 
 type AddressableDao interface {
-	Get(id string) (Addressable, error)
-	GetAll() ([]Addressable, error)
-	Exists(id string) (bool, error)
-	Add(addressable Addressable) (string, error)
-	Update(addressable Addressable) error
-	Remove(id string) error
+	Get(id string, dbClient DBClient) (Addressable, error)
+	GetAll(dbClient DBClient) ([]Addressable, error)
+	Exists(id string, dbClient DBClient) (bool, error)
+	Add(addressable Addressable, dbClient DBClient) (string, error)
+	Update(addressable Addressable, dbClient DBClient) error
+	Remove(id string, dbClient DBClient) error
+}
+
+type DatabaseType int8 // Database type enum
+const (
+	MONGO DatabaseType = iota
+)
+
+type DBConfiguration struct {
+	DbType       DatabaseType
+	Host         string
+	Port         int
+	Timeout      int
+	DatabaseName string
+	Username     string
+	Password     string
+}
+
+var ErrNotFound = errors.New("Item not found")
+var ErrUnsupportedDatabase = errors.New("Unsuppored database type")
+var ErrInvalidObjectId = errors.New("Invalid object ID")
+var ErrNotUnique = errors.New("Resource already exists")
+
+type DBClient interface {
+	OpenSession() error
+	CloseSession()
+}
+
+// Return the dbClient interface
+func NewDBClient(config DBConfiguration) (DBClient, error) {
+	switch config.DbType {
+	case MONGO:
+		// Create the mongo client
+		return newMongoClient(config)
+	default:
+		return nil, ErrUnsupportedDatabase
+	}
 }
