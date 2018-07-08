f681ecbd8137506806f269b6b2c52dd4d05d9a62
yanghua <yanghua1127@gmail.com>
2018-07-08 12:34:30 +0800

    #60 Use GatewayDao in GatewayController
    Signed-off-by: yanghua <yanghua1127@gmail.com>

--- web/app/cache.go
+++ web/app/cache.go
@@ -17,9 +17,6 @@ package app
 //{Token:User}
 var TokenCache = make(map[string]User, 20)
 
-//{gatewayID:Gateway}
-var GatewayInfoCache = make([]Gateway, 0)
-
 //target ProxyCache {token:targetIP}
 var DynamicalProxyCache = make(map[string]string, 10)
 
