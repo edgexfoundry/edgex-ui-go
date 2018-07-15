b5daa2c80589df718110f958f7a548c1b9fedd28
yanghua <yanghua1127@gmail.com>
2018-07-15 21:23:08 +0800

    #112 Rename controller methods
    Signed-off-by: yanghua <yanghua1127@gmail.com>

--- web/app/router.go
+++ web/app/router.go
@@ -28,9 +28,9 @@ func InitRestRoutes() http.Handler {
 	s.HandleFunc("/auth/login", controller.Login).Methods(http.MethodPost)
 	s.HandleFunc("/auth/logout", controller.Logout).Methods(http.MethodGet)
 
-	s.HandleFunc("/gateway", controller.FindAllGateway).Methods(http.MethodGet)
-	s.HandleFunc("/gateway", controller.SaveGateway).Methods(http.MethodPost)
-	s.HandleFunc("/gateway/{id}", controller.DeleteGateway).Methods(http.MethodDelete)
+	s.HandleFunc("/gateway", controller.QueryAllGateway).Methods(http.MethodGet)
+	s.HandleFunc("/gateway", controller.AddGateway).Methods(http.MethodPost)
+	s.HandleFunc("/gateway/{id}", controller.RemoveGateway).Methods(http.MethodDelete)
 	s.HandleFunc("/gateway/proxy", controller.ProxyConfigGateway).Methods(http.MethodPost)
 
 	s.HandleFunc("/exportshow", controller.ExportShow).Methods(http.MethodPost)
