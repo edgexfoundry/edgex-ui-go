bb3c727c9975366449bf64cb106654647889a500
Huaqiao Zhang <huaqiaoz@vmware.com>
2018-12-04 22:23:27 +0800

    issue-#57 ruleengine improved using panel wrapper
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/static/js/pages/ruleengine.js
+++ web/static/js/pages/ruleengine.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	$("#condition_device_list table").hide();
 	$("#action_device_list table").hide();
 	$("#device_command_list table").hide();
-	ruleEngineModule.loadRuleEngineData();
+	orgEdgexFoundry.supportRuleEngine.loadRuleData();
 
 	//global listener for hiding device-list section.
 	document.addEventListener('click',function(event){
@@ -50,9 +50,9 @@ $(document).ready(function(){
 		url:'/core-metadata/api/v1/device',
 		type:'GET',
 		success:function(data){
-			ruleEngineModule.deviceDataCache = data;
-			$("#action_device_list table > tbody").empty();
-			$("#condition_device_list table > tbody").empty();
+			orgEdgexFoundry.supportRuleEngine.deviceDataCache = data;
+			$("#action_device_list table tbody").empty();
+			$("#condition_device_list table tbody").empty();
 			$.each(data,function(i,d){
 				var rowData = "<tr>";
 				rowData += "<td><input type='radio' name='condition' value= '" + d.id + "'></td>";
@@ -60,13 +60,13 @@ $(document).ready(function(){
 				rowData += "<td>" + d.id.substr(0,8) + "</td>";
 				rowData += "<td>" + d.name + "</td>";
 				rowData += "</tr>";
-				$("#action_device_list table > tbody").append(rowData);
-				$("#condition_device_list table > tbody").append(rowData);
+				$("#action_device_list table  tbody").append(rowData);
+				$("#condition_device_list table  tbody").append(rowData);
 			});
-			$("#condition_device_list table > tbody input:radio").on('click',function(){
+			$("#condition_device_list table  tbody input:radio").on('click',function(){
 				if($(this).prop('checked')){
 					var radio = this;
-					$.each(ruleEngineModule.deviceDataCache,function(i,d){
+					$.each(orgEdgexFoundry.supportRuleEngine.deviceDataCache,function(i,d){
 						if(d.id == $(radio).val()) {
 							//$("#condition_device_list input").val();
 							$("#condition_device_list div.select_panle input[name='condition_device_name']").val(d.name);
@@ -83,7 +83,7 @@ $(document).ready(function(){
 			$("#action_device_list table > tbody input:radio").on('click',function(){
 				if($(this).prop('checked')){
 					var radio = this;
-					$.each(ruleEngineModule.deviceDataCache,function(i,d){
+					$.each(orgEdgexFoundry.supportRuleEngine.deviceDataCache,function(i,d){
 						if(d.id == $(radio).val()) {
 							$("#action_device_list div.select_panle input[name='action_device_name']").val(d.name);
 							$(".action_device_list select[name='commandName']").empty();
@@ -104,7 +104,7 @@ $(document).ready(function(){
 				$("#action_device_param").empty();
 				var cmdId = $(this).val();
 				var flag = true;
-				$.each(ruleEngineModule.deviceDataCache,function(i,d){
+				$.each(orgEdgexFoundry.supportRuleEngine.deviceDataCache,function(i,d){
 					$.each(d.profile.commands,function(k,cmd){
 						if(cmd.id == cmdId){
 							if(cmd.put == null){
@@ -126,117 +126,198 @@ $(document).ready(function(){
 	});
 });
 
+orgEdgexFoundry.supportRuleEngine = (function(){
+	"use strict";
+
+	function SupportRuleEngine() {
+		this.selectRule = null;
+		this.ruleEngineDataCache = [];
+		this.deviceDataCache = [];
+	}
+
+	SupportRuleEngine.prototype = {
+		loadRuleData: null,
+		renderRuleList: null,
+
+		addRule: null,
+		cancelAdd: null,
+		commitRule: null,
+		deleteRule: null,
+	}
+
+	var ruleEngine = new SupportRuleEngine();
+
+	SupportRuleEngine.prototype.loadRuleData = function(){
+		$.ajax({
+			url:'/rule-engine/api/v1/rule',
+			type:'GET',
+			success:function(data){
+				if(data != null && data.length != 0){
+					$("#rule-engine-list table tfoot").hide();
+				}
+				ruleEngine.renderRuleList(data);
+			}
+		});
+	}
+
+	SupportRuleEngine.prototype.renderRuleList = function(data){
+		$("#rule-engine-list table tbody").empty();
+		$.each(data,function(i,rule){
+			var rowData = "<tr>";
+			rowData += "<td><input type='radio' name='ruleRadio' value='"+rule+"'></td>";
+			rowData += "<td>" + (i+1) + "</td>";
+			rowData += "<td>" + "--" + "</td>";
+			rowData += "<td>" + rule + "</td>";
+			rowData += "<td>" + "--" + "</td>";
+			rowData += "<td>" + "--" + "</td>";
+			$("#rule-engine-list table tbody").append(rowData);
+		});
+		$("#rule-engine-list table tbody input:radio").on('click',function(){
+			if($(this).prop('checked')){
+				ruleEngine.selectRule = $(this).val();
+			}
+		});
+	}
+
+	SupportRuleEngine.prototype.addRule = function(){
+		$("#rule-engine-list").hide();
+		$("#rule-add-new").show();
+	}
+
+	SupportRuleEngine.prototype.cancelAdd = function(){
+		$("#rule-engine-list").show();
+		$("#rule-add-new").hide();
+	}
+
+	SupportRuleEngine.prototype.commitRule = function(){
+		var newRule = {};
+		var condition = {};
+		var checks = [];
+		var action = {};
+		var name = $("#rule-add-new input[name='name']").val();
+		var condition_device_name = $("#rule-add-new input[name='condition_device_name']").val();
+		var parameter = $("#rule-add-new select[name='parameter']").val();
+		//var operand1 =
+		var operation =  $("#rule-add-new select[name='operation']").val();
+		var operand2 =  $("#rule-add-new input[name='operand2']").val();
+
+		var action_device_name = $("#action_device_list input[name='action_device_name']").val();
+		var command = $("#rule-add-new  select[name='commandName']").val();
+		var body = '{\\\"';
+		var param_inputs = $("table.action_device_list td").last().find('input');
+		$.each(param_inputs,function(i,input){
+			body += $(input).prop('name')+'\\\":\\\"' + $(input).val() + '\\\"';
+			if(i == (param_inputs.length - 1)){
+				body += '}';
+				return false;
+			}
+			body += ',\\\"';
+		});
+
+		condition['device'] = condition_device_name;
+		checks.push({"parameter":parameter,
+					"operand1":"Float.parseFloat(value)",
+					"operation":operation,"operand2":operand2});
+		condition['checks'] = checks;
+		$.each(ruleEngineModule.deviceDataCache,function(i,d){
+			if(d.name == action_device_name){
+				action['device'] = d.id;
+			}
+		});
+		action['command'] = command;
+		action['body'] = body;
+		newRule['name'] = name;
+		newRule['condition'] = condition;
+		newRule['action'] = action;
+		newRule['log'] = "";
+		console.log(JSON.stringify(newRule));
+		$.ajax({
+			url:'/rule-engine/api/v1/rule',
+			type:'POST',
+			data:JSON.stringify(newRule),
+			contentType:'application/json',
+			success:function(){
+				$("#rule-add-new").hide();
+				$("#rule-engine-list").show();
+				ruleEngine.loadRuleData();
+			}
+		});
+	}
+
+	SupportRuleEngine.prototype.deleteRule = function(){
+		if (!ruleEngine.selectRule) {
+			bootbox.alert({
+				message: "Please select one item !",
+				className: 'red-green-buttons'
+			});
+			return;
+		}
+		bootbox.confirm({
+			title: "confirm",
+			message: "Are you sure to delete ? ",
+			className: 'green-red-buttons',
+			callback: function (result) {
+				if (result) {
+					$.ajax({
+						url:'/rule-engine/api/v1/rule/name/'+ruleEngineModule.selectRule+'',
+						type: 'DELETE',
+						success: function(){
+							bootbox.alert({
+								message: "Delete Success!",
+								className: 'red-green-buttons'
+							});
+							ruleEngineModule.loadRuleEngineData();
+						},
+						statusCode: {
+							404: function(){
+								bootbox.alert({
+									title:'Error',
+									message: "unknown or unanticipated issues !",
+									className: 'red-green-buttons'
+								});
+							},
+							503: function(){
+								bootbox.alert({
+									title:'Error',
+									message: "unknown or unanticipated issues !",
+									className: 'red-green-buttons'
+								});
+							}
+						}
+					});
+				}
+			}
+		});
+	}
+
+	return ruleEngine;
+})();
+
 var ruleEngineModule = {
 		selectRule:"",
 		ruleEngineDataCache:[],
 		deviceDataCache:[],
 		loadRuleEngineData:function(){
-			$.ajax({
-				url:'/rule-engine/api/v1/rule',
-				type:'GET',
-				success:function(data){
-					if(data != null && data.length != 0){
-						$("#rule-engine-list table tfoot").hide();
-					}
-					ruleEngineModule.renderList(data);
-				}
-			});
+
 		},
 		renderList:function(data){
-			$("#rule-engine-list table tbody").empty();
-			$.each(data,function(i,rule){
-				var rowData = "<tr>";
-				rowData += "<td><input type='radio' name='ruleRadio' value='"+rule+"'></td>";
-				rowData += "<td>" + (i+1) + "</td>";
-				rowData += "<td>" + "--" + "</td>";
-				rowData += "<td>" + rule + "</td>";
-				rowData += "<td>" + "--" + "</td>";
-				rowData += "<td>" + "--" + "</td>";
-				$("#rule-engine-list table tbody").append(rowData);
-			});
-			$("#rule-engine-list table tbody input:radio").on('click',function(){
-				if($(this).prop('checked')){
-					ruleEngineModule.selectRule = $(this).val();
-				}
-			});
+
 		}
 }
 var ruleEngineModuleBtnGroup = {
 		add:function(){
-			$("#rule-engine-list").hide();
-			$("#rule-add-new").show();
+
 		},
 		deleteRule:function(confirm){
-			$('#deleteConfirmDialog').modal('show');
-			if(confirm){
-				$('#deleteConfirmDialog').modal('hide');
-				$.ajax({
-					url:'/rule-engine/api/v1/rule/name/'+ruleEngineModule.selectRule+'',
-					type:'DELETE',
-					success:function(){
-						ruleEngineModule.loadRuleEngineData();
-					}
-				});
-			}
+
+
 		},
 		detail:function(){},
 		back:function(){
-			$("#rule-engine-list").show();
-			$("#rule-add-new").hide();
+
 		},
 		submit:function(){
-			var newRule = {};
-			var condition = {};
-			var checks = [];
-			var action = {};
-			var name = $("#rule-add-new input[name='name']").val();
-			var condition_device_name = $("#rule-add-new input[name='condition_device_name']").val();
-			var parameter = $("#rule-add-new select[name='parameter']").val();
-			//var operand1 =
-			var operation =  $("#rule-add-new select[name='operation']").val();
-			var operand2 =  $("#rule-add-new input[name='operand2']").val();
 
-			var action_device_name = $("#action_device_list input[name='action_device_name']").val();
-			var command = $("#rule-add-new  select[name='commandName']").val();
-			var body = '{\\\"';
-			var param_inputs = $("table.action_device_list td").last().find('input');
-			$.each(param_inputs,function(i,input){
-				body += $(input).prop('name')+'\\\":\\\"' + $(input).val() + '\\\"';
-				if(i == (param_inputs.length - 1)){
-					body += '}';
-					return false;
-				}
-				body += ',\\\"';
-			});
-
-			condition['device'] = condition_device_name;
-			checks.push({"parameter":parameter,
-						"operand1":"Float.parseFloat(value)",
-						"operation":operation,"operand2":operand2});
-			condition['checks'] = checks;
-			$.each(ruleEngineModule.deviceDataCache,function(i,d){
-				if(d.name == action_device_name){
-					action['device'] = d.id;
-				}
-			});
-			action['command'] = command;
-			action['body'] = body;
-			newRule['name'] = name;
-			newRule['condition'] = condition;
-			newRule['action'] = action;
-			newRule['log'] = "just test.";
-			console.log(JSON.stringify(newRule));
-			$.ajax({
-				url:'/rule-engine/api/v1/rule',
-				type:'POST',
-				data:JSON.stringify(newRule),
-				contentType:'application/json',
-				success:function(){
-					$("#rule-add-new").hide();
-					$("#rule-engine-list").show();
-					ruleEngineModule.loadRuleEngineData();
-				}
-			});
 		},
 		update:function(){}
 }
