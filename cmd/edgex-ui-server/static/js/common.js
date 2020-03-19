88705bb67862d4571a83738d733e5d0b2e31b868
zhanglibin <zlibin@vmware.com>
2020-03-19 12:28:38 +0800

    Update Interval part in scheduler page to match edgexfoundry
    issue #168
    Signed-off-by: zhanglibin <zlibin@vmware.com>

--- cmd/edgex-ui-server/static/js/common.js
+++ cmd/edgex-ui-server/static/js/common.js
@@ -33,4 +33,21 @@ var dateToString = function (num){
 
 	var str = y + '-' + M + '-' + d + ' ' + hh + ':' + mm + ':' + ss
 	return str;
-}
+};
+
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, // Month
+        "d+": this.getDate(), // Day
+        "h+": this.getHours(), // Hour
+        "m+": this.getMinutes(), // Minute
+        "s+": this.getSeconds(), // Second
+        "q+": Math.floor((this.getMonth() + 3) / 3), // Quarter
+        "S": this.getMilliseconds() // Millisecond
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
