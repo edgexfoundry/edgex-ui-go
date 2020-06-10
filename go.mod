fc6b590da5db28411c1d4ed520be24f88b5c953f
zhanglibin <zlibin@vmware.com>
2020-06-10 19:37:01 +0800

    [feat] Add override environment support
    Issue #236
    Signed-off-by: zhanglibin <zlibin@vmware.com>

--- go.mod
+++ go.mod
@@ -3,24 +3,23 @@ module github.com/edgexfoundry/edgex-ui-go
 go 1.13
 
 require (
-	github.com/BurntSushi/toml v0.3.1
 	github.com/armon/go-metrics v0.3.3 // indirect
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/edgexfoundry/go-mod-registry v0.1.11
-	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/gorilla/mux v1.7.1
 	github.com/gorilla/websocket v1.4.0
-	github.com/hashicorp/consul v1.7.2 // indirect
 	github.com/hashicorp/consul/api v1.4.0 // indirect
 	github.com/hashicorp/go-hclog v0.12.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.2.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/serf v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mitchellh/mapstructure v1.2.2 // indirect
-	github.com/pebbe/zmq4 v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.1 // indirect
 	github.com/pelletier/go-toml v1.7.0
-	go.mongodb.org/mongo-driver v1.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20191106202628-ed6320f186d4 // indirect
 	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
---
+++ internal/configs/environment.go
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright © 2018-2020 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *******************************************************************************/
+
+package configs
+
+import (
+	"os"
+	"strings"
+	"github.com/pelletier/go-toml"
+	"reflect"
+	"strconv"
+)
+
+type environment struct {
+	env map[string]interface{}
+}
+
+func NewEnvironment() *environment {
+	osEnv := os.Environ()
+	e := &environment{
+		env: make(map[string]interface{}, len(osEnv)),
+	}
+	for _, e1 := range osEnv {
+		kv := strings.Split(e1, "=")
+		if len(kv) == 2 && len(kv[0]) > 0 && len(kv[1]) > 0 {
+			e.env[kv[0]] = kv[1]
+		}
+	}
+	return e
+}
+
+func (e *environment) OverrideFromEnvironment(tree *toml.Tree) (*toml.Tree, error) {
+	for k, v := range e.env {
+		k = strings.Replace(k, "_", ".", -1)
+		if tree.Has(k) {
+			// global key
+			realType := reflect.TypeOf(tree.Get(k)).Kind()
+			switch realType {
+			case reflect.Int64:
+				v, err := strconv.ParseInt(v.(string), 10, 64)
+				if err != nil {
+					return nil, err
+				}
+				tree.Set(k, v)
+				break
+			case reflect.Int:
+				v, err := strconv.Atoi(v.(string))
+				if err != nil {
+					return nil, err
+				}
+				tree.Set(k, v)
+				break
+			default:
+				tree.Set(k, v)
+			}
+		}
+	}
+	return tree, nil
+}
