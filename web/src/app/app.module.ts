14f4736fecbb0a1c2eea740cf2bad9dc2b5febc0
Huaqiao Zhang <huaqiaoz@vmware.com>
2020-12-14 18:08:06 +0800

    Add global httpclient module
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
@@ -8,6 +9,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
 =======
+=======
+import { HttpClientModule } from '@angular/common/http';
+>>>>>>> f21e84a... Add global httpclient module
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,8 +35,13 @@ import { MesssageComponent } from './message/messsage.component';
   ],
   imports: [
     BrowserModule,
+<<<<<<< HEAD
     AppRoutingModule
 >>>>>>> d08a9c7... init scaffold
+=======
+    AppRoutingModule,
+    HttpClientModule
+>>>>>>> f21e84a... Add global httpclient module
   ],
   providers: [],
   bootstrap: [AppComponent]
