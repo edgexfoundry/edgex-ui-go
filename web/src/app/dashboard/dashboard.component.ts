e356c1eb2e8462874c2fb5ccc8e7aa231d08e506
Huaqiao Zhang <huaqiaoz@vmware.com>
2020-12-14 20:11:47 +0800

    init dashboard module
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>
    Signed-off-by: wshengqi@vmware.com <wshengqi@vmware.com>

--- web/src/app/dashboard/dashboard.component.ts
+++ web/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service'
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  eventCount?: number = 0;
+  readingCount?: number = 0;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.eventCount().subscribe((data: number) => this.eventCount = data)
+    this.dataService.readingCount().subscribe((data: number) => this.readingCount = data)
   }
 
 }
