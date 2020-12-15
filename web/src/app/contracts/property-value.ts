fd4961e83d3a882af5956d8cf914e2d1a22ba3f4
Huaqiao Zhang <huaqiaoz@vmware.com>
2020-12-15 21:26:13 +0800

    Device and DeviceService program models completed
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>
    Signed-off-by: wshengqi@vmware.com <wshengqi@vmware.com>

---
+++ web/src/app/contracts/property-value.ts
@@ -0,0 +1,17 @@
+export interface PropertyValue {
+    type?: string,
+    readWrite?: string,
+    minimum?: string,
+    maximum?: string,
+    defaultValue?: string,
+    size?: string,
+    mask?: string,
+    shift?: string,
+    scale?: string,
+    offset?: string,
+    base?: string,
+    assertion?: string,
+    precision?: string,
+    floatEncoding?: string,
+    mediaType?: string
+}
