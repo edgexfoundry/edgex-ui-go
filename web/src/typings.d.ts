4f72167aef4f3ea149941f0c0a8eaa763996e979
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-01-05 11:26:34 +0800

    add javascript library
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/typings.d.ts
+++ web/src/typings.d.ts
@@ -1,4 +1,8 @@
 declare var $: any;
+<<<<<<< HEAD
 declare var echarts: any;
 declare var CodeMirror: any;
 declare var CBOR: any;
+=======
+declare var echarts: any;
+>>>>>>> b83cdaa... add javascript library
