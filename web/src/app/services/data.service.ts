dd2f83196ec43702c3034e51912ff5881c44ea74
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-01-05 16:18:15 +0800

    update and add more http service
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>
    Signed-off-by: wshengqi@vmware.com <wshengqi@vmware.com>

--- web/src/app/services/data.service.ts
+++ web/src/app/services/data.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  endpoint: string = "/coredata";
+
+  endpointHealthUrl: string = "/api/v1/ping";
+
+  eventCountUrl: string = "/api/v1/event/count";
+  readingCountUrl: string = "/api/v1/reading/count";
+
+  constructor(private http: HttpClient) { }
+
+  endpointHealth(): Observable<string> {
+    let url = `${this.endpoint}${this.endpointHealthUrl}`;
+    return this.http.get<string>(url)
+  }
+
+  eventCount(): Observable<number> {
+    let url = `${this.endpoint}${this.eventCountUrl}`;
+    return this.http.get<number>(url)
+  }
+
+  readingCount(): Observable<number> {
+    let url = `${this.endpoint}${this.readingCountUrl}`;
+    return this.http.get<number>(url)
+  }
+
 }
