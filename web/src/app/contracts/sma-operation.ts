8caa004cf67ad5c7ff9864f703cdc163a21fa1bc
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-01-05 15:58:15 +0800

    Add sma operation interface
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/contracts/sma-operation.ts
+++ web/src/app/contracts/sma-operation.ts
@@ -1,7 +1,12 @@
 import { Action } from "./action";
 
 export interface SmaOperation {
+<<<<<<< HEAD
     action: string,
     services: string[],
     params?: string[]
+=======
+    action: Action,
+    services: string[]
+>>>>>>> d6fa576... Add sma operation interface
 }
