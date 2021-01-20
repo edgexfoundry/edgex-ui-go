07c86ccfa16ac2df36a904b606567c69126454dd
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-01-20 17:37:58 +0800

    Add system message bus module and update some function
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/message/message.component.ts
+++ web/src/app/message/message.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MessageService } from './message.service';
+<<<<<<< HEAD
 import { Message } from './message.service'
+=======
+>>>>>>> d1c0229... Add system message bus module and update some function
 
 @Component({
   selector: 'app-message',
@@ -14,11 +17,15 @@ export class MessageComponent implements OnInit, OnDestroy {
   constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
+<<<<<<< HEAD
     // this.timer = setInterval(() => {this.messageService.clear() }, 3000)
   }
 
   close(msg: Message) {
     this.messageService.messages.splice(this.messageService.messages.indexOf(msg), 1)
+=======
+    this.timer = setInterval(() => { this.messageService.clear() }, 3000)
+>>>>>>> d1c0229... Add system message bus module and update some function
   }
 
   ngOnDestroy(): void {
@@ -26,4 +33,8 @@ export class MessageComponent implements OnInit, OnDestroy {
       clearInterval(this.timer);
     }
   }
+<<<<<<< HEAD
+=======
+
+>>>>>>> d1c0229... Add system message bus module and update some function
 }
