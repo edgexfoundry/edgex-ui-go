784c9d8113ad116573f78e3d84a1371c9fb9a73c
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-01-20 17:40:43 +0800

    Finished system agent module basic feature
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/system-agent/system-agent.module.ts
+++ web/src/app/system-agent/system-agent.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 import { SystemAgentRoutingModule } from './system-agent-routing.module';
 import { SystemAgentComponent } from './system-agent.component';
+import { MetricsComponent } from './metrics/metrics.component';
+import { ConfigComponent } from './config/config.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 
 @NgModule({
-  declarations: [SystemAgentComponent],
+  declarations: [SystemAgentComponent, MetricsComponent, ConfigComponent, ServiceListComponent],
   imports: [
     CommonModule,
+    FormsModule,
     SystemAgentRoutingModule
   ]
 })
