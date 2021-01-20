1e020238794e4dd3ce9cf9070d49fdd576fa3365
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-01-20 17:38:59 +0800

    Update app routing and import others module format
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>
    Signed-off-by: wshengqi@vmware.com <wshengqi@vmware.com>

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MesssageComponent } from './message/messsage.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MesssageComponent
+    MessageComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+
     AppRoutingModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
