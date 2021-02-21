67bf16503984c211fd0b3c1c9208bd9812383038
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-02-21 16:42:29 +0800

    Update App component for sidebar responsive layout
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -12,13 +12,19 @@ export class AppComponent implements OnInit {
   title = 'EdgeX Console';
   isSelected: boolean = false;
   toggleClass: string = '';
+  shrink: boolean = false;
+  shrinkSidebarOnly: boolean = false;
+  shrinkCenterNo: boolean = false;
+
+
   navEnd: Observable<NavigationEnd>;
   navStart: Observable<NavigationStart>;
   navRecognized: Observable<RoutesRecognized>;
 
   childStart: Observable<ChildActivationStart>;
 
-  navChainCache = new Map();
+  navChainMap = new Map<string, string>();
+  navChainCache: string[] = [];
   navChain: string[] = [];
   currentNav?: string;
 
@@ -47,41 +53,64 @@ export class AppComponent implements OnInit {
     // this.activatedRoute.url
     //   .subscribe(url => console.log('The URL changed to: ' + url));
     // this.navStart.subscribe(evt => console.log(evt));
-    this.navRecognized.subscribe(evt => console.log(evt.state.root.pathFromRoot));
+    // this.navRecognized.subscribe(evt => console.log(evt.state.root.pathFromRoot));
 
     // this.childStart.subscribe(evt => console.log(evt));
 
 
     this.navEnd.subscribe(evt => {
-      console.log(evt);
+      this.navChainCache = [];
+      this.navChainMap = new Map<string, string>();
+      // console.log(evt);
 
-      this.currentNav = evt.urlAfterRedirects.split('/').pop() as string;
+      this.currentNav = evt.urlAfterRedirects.split('?')[0].split('/').pop() as string;
       // this.navChainCache.set(last, evt.urlAfterRedirects);
-      this.navChain = evt.urlAfterRedirects.split('/');
+      this.navChain = evt.urlAfterRedirects.split('?')[0].split('/');
       this.navChain.shift();
       // this.currentNav = this.navChain.slice(0, 1)[0];
       // console.log(this.navChain);
+      let self = this;
+      let reverseNav1 = this.navChain.map(x => x).reverse();
+      // console.log(this.navChain);
+      // console.log(reverseNav1)
+      reverseNav1.forEach(function (item, index) {
+        // console.log(item, index)
+        let reverseNav2 = self.navChain.map(x => x).reverse();
+        let t = reverseNav2.slice(index)
+        // console.log(t)
+        // console.log(t.reverse().push(item))
+        let nav = t.reverse().join('/');
+        // console.log(nav)
+        self.navChainCache.push("/" + nav)
+        self.navChainMap.set(item, "/" + nav)
+      });
+      this.navChainCache.reverse();
+      // console.log(this.navChainCache)
+      // console.log(this.navChainMap)
+      // console.log(this.navChain)
     });
 
-    let self = this;
-
-    // $(window).resize(function () {
-    //   let window_width = $(window).width();
-    //   console.log(window_width)
-    //   if (window_width < 1390) {
-    //     self.toggleClass = 'd-sm-none';
-    //   } else {
-    //     self.toggleClass = '';
-    //   }
-    // });
+    $(window).resize(() => {
+      this.shrinkCenterNo = false;
+      if (document.body.clientWidth < 1340) {
+        this.shrink = true;
+      } else {
+        this.shrink = false;
+      }
+    });
   }
 
-  autoHeight() {
-    let winHeight = $(document).height();//winHeight即浏览器高度
-    console.log(winHeight)
-    let menuHeight = $(".sidebar-center").offsetHeight;//菜单高度;其中menu_div为菜单所在标签的id
-    if (menuHeight = winHeight) {
-      $(".sidebar-center").css("height", "auto");
+  sideBarToggle() {
+    if (this.shrink) {
+      if (document.body.clientWidth < 1340) {
+        this.shrinkCenterNo = true;
+      }
+      this.shrink = false;
+    } else {
+      if (document.body.clientWidth < 1340) {
+        this.shrinkCenterNo = false;
+      }
+      this.shrink = true;
     }
   }
 }
