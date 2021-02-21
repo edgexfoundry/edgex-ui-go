c5da1da506084f9a752947742e1ef6a36f0b7a9a
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-02-21 16:55:42 +0800

    Update url splicing formate with version variable
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>
    Signed-off-by: wshengqi@vmware.com <wshengqi@vmware.com>

--- web/src/app/services/metadata.service.ts
+++ web/src/app/services/metadata.service.ts
@@ -11,24 +11,32 @@ import { DeviceService } from '../contracts/device-service';
 export class MetadataService {
 
   endpoint: string = "/metadata";
-  devicesListUrl: string = "/api/v1/device";
-  deleteOneDeviceUrl: string = "/api/v1/device";
-  updateOneDeviceUrl: string = "/api/v1/device";
-  addOneDeviceUrl: string = "/api/v1/device";
-  findDeviceByNameUrl: string = "/api/v1/device/name";
-  findDevicesByServiceIdUrl: string = "/api/v1/device/service";
-  findDevicesByProfileIdUrl: string = "/api/v1/device/profile";
+  version: string = "/api/v1";
+
+  devicesListUrl: string = `${this.endpoint}${this.version}/device`;
+  deleteOneDeviceUrl: string = `${this.endpoint}${this.version}/device`;
+  updateOneDeviceUrl: string = `${this.endpoint}${this.version}/device`;
+  addOneDeviceUrl: string = `${this.endpoint}${this.version}/device`;
+  findDeviceByNameUrl: string = `${this.endpoint}${this.version}/device/name`;
+  findDeviceByIdUrl: string = `${this.endpoint}${this.version}/device`;
+  findDevicesByServiceIdUrl: string = `${this.endpoint}${this.version}/device/service`;
+  findDevicesByProfileIdUrl: string = `${this.endpoint}${this.version}/device/profile`;
 
 
-  deviceServicesListUrl: string = "/api/v1/deviceservice";
-  updateDeviceServiceUrl: string = "/api/v1/deviceservice";
-  findDeviceServiceByIdUrl: string = "/api/v1/deviceservice";
+  deviceServicesListUrl: string = `${this.endpoint}${this.version}/deviceservice`;
+  updateDeviceServiceUrl: string = `${this.endpoint}${this.version}/deviceservice`;
+  findDeviceServiceByIdUrl: string = `${this.endpoint}${this.version}/deviceservice`;
 
 
-  deviceProfilesListUrl: string = "/api/v1/deviceprofile";
-  updateDeviceProfileUrl: string = "/api/v1/deviceprofile";
+  deviceProfilesListUrl: string = `${this.endpoint}${this.version}/deviceprofile`;
+  findProfilesByIdUrl: string = `${this.endpoint}${this.version}/deviceprofile`;
+  updateDeviceProfileUrl: string = `${this.endpoint}${this.version}/deviceprofile`;
+  uploadProfileYamlFileUrl: string = `${this.endpoint}${this.version}/deviceprofile/uploadfile`;
+  uploadProfileYamlContentUrl: string = `${this.endpoint}${this.version}/deviceprofile/upload`;
+  deviceProfileYamlUrl: string = `${this.endpoint}${this.version}/deviceprofile/yaml/`;
+  deleteProfileByIdUrl: string = `${this.endpoint}${this.version}/deviceprofile/id/`;
 
-  httpPostOrPutOptions = {
+  httpPostOrPutJSONOptions = {
     headers: new HttpHeaders({
       'Content-type': 'application/json',
       'Authorization': ''
@@ -40,66 +48,127 @@ export class MetadataService {
   //Device resources
 
   addDevice(device: Device): Observable<string> {
-    let url = `${this.endpoint}${this.addOneDeviceUrl}`;
-    return this.http.post<string>(url, device, this.httpPostOrPutOptions)
+    let url = `${this.addOneDeviceUrl}`;
+    return this.http.request('POST', url, {
+      body: JSON.stringify(device),
+      responseType: 'text',
+      headers: new HttpHeaders({
+        'Content-type': 'application/json'
+      })
+    })
   }
 
   deleteOneDeviceById(id: string): Observable<any> {
-    let url = `${this.endpoint}${this.deleteOneDeviceUrl}/${id}`;
+    let url = `${this.deleteOneDeviceUrl}/id/${id}`;
     return this.http.delete(url)
   }
 
   updateDevice(device: Device): Observable<any> {
-    let url = `${this.endpoint}${this.updateOneDeviceUrl}`;
-    return this.http.put(url, device, this.httpPostOrPutOptions)
+    let url = `${this.updateOneDeviceUrl}`;
+    return this.http.request('PUT', url, {
+      body: JSON.stringify(device),
+      responseType: 'text',
+      headers: new HttpHeaders({
+        'Content-type': 'application/json'
+      })
+    })
+    // return this.http.put(url, device, this.httpPostOrPutJSONOptions)
   }
 
   findDeviceByName(name: string): Observable<Device> {
-    let url = `${this.endpoint}${this.findDeviceByNameUrl}/${name}`;
+    let url = `${this.findDeviceByNameUrl}/${name}`;
     return this.http.get<Device>(url)
   }
 
+  findDeviceById(id: string): Observable<Device> {
+    let url = `${this.findDeviceByIdUrl}/${id}`;
+    return this.http.get<Device>(url)
+  }
+
+
   allDevices(): Observable<Device[]> {
-    let url = `${this.endpoint}${this.devicesListUrl}`;
+    let url = `${this.devicesListUrl}`;
     return this.http.get<Device[]>(url)
   }
 
   findDevicesByServiceId(serviceId: string): Observable<Device[]> {
-    let url = `${this.endpoint}${this.findDevicesByServiceIdUrl}/${serviceId}`;
+    let url = `${this.findDevicesByServiceIdUrl}/${serviceId}`;
     return this.http.get<Device[]>(url)
   }
 
   findDevicesByProfileId(profileId: string): Observable<Device[]> {
-    let url = `${this.endpoint}${this.findDevicesByProfileIdUrl}/${profileId}`;
+    let url = `${this.findDevicesByProfileIdUrl}/${profileId}`;
     return this.http.get<Device[]>(url)
   }
 
   //Device Service resources
 
   allDeviceServices(): Observable<DeviceService[]> {
-    let url = `${this.endpoint}${this.deviceServicesListUrl}`;
+    let url = `${this.deviceServicesListUrl}`;
     return this.http.get<DeviceService[]>(url)
   }
 
   updateDeviceService(deviceService: DeviceService): Observable<any> {
-    let url = `${this.endpoint}${this.updateDeviceServiceUrl}`;
-    return this.http.put(url, deviceService, this.httpPostOrPutOptions)
+    let url = `${this.updateDeviceServiceUrl}`;
+    return this.http.put(url, deviceService, this.httpPostOrPutJSONOptions)
   }
 
   findDevcieServiceById(id: string): Observable<DeviceService> {
-    let url = `${this.endpoint}${this.findDeviceServiceByIdUrl}/${id}`;
+    let url = `${this.findDeviceServiceByIdUrl}/${id}`;
     return this.http.get<DeviceService>(url)
   }
 
   //Device Profile resources
 
   allDeviceProfoles(): Observable<DeviceProfile[]> {
-    let url = `${this.endpoint}${this.deviceProfilesListUrl}`;
+    let url = `${this.deviceProfilesListUrl}`;
     return this.http.get<DeviceProfile[]>(url)
   }
 
+  findProfileById(id: string): Observable<DeviceProfile> {
+    let url = `${this.findProfilesByIdUrl}/${id}`;
+    return this.http.get<DeviceProfile>(url)
+  }
+
+  uploadProfileYamlFile(data: any): Observable<any> {
+    let url = `${this.uploadProfileYamlFileUrl}`;
+    return this.http.request('POST', url, {
+      body: data,
+      responseType: 'text',
+      // headers: new HttpHeaders({
+      //   'Content-Type': 'multipart/form-data; charset=utf-8'
+      // })
+    })
+  }
+
   updateDeviceProfile(profile: DeviceProfile): Observable<any> {
-    let url = `${this.endpoint}${this.updateDeviceProfileUrl}`;
-    return this.http.put(url, profile, this.httpPostOrPutOptions)
+    let url = `${this.updateDeviceProfileUrl}`;
+    return this.http.put(url, profile, this.httpPostOrPutJSONOptions)
+  }
+
+  // updateProfileYamlContent():Observable<any> {
+  //   let url = `${this.endpoint}${this.uploadProfileYamlContentUrl}`;
+  // }
+
+  uploadProfileYamlContent(data: any): Observable<string> {
+    let url = `${this.uploadProfileYamlContentUrl}`;
+    return this.http.request('POST', url, {
+      body: data,
+      responseType: 'text',
+      headers: new HttpHeaders({
+        'Content-Type': 'text/plain; charset=utf-8'
+      })
+    })
+  }
+
+  //deprecated
+  findProfileYamlById(id: string): Observable<any> {
+    let url = `${this.deviceProfileYamlUrl}${id}`;
+    return this.http.request('GET', url, { responseType: 'text' })
+  }
+
+  deleteProfileById(id: string): Observable<any> {
+    let url = `${this.deleteProfileByIdUrl}${id}`;
+    return this.http.delete(url)
   }
 }
