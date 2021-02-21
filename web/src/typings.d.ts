8ec8e74a269d3436d87ec83e0d20c265690087dd
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-02-21 16:43:28 +0800

    Update system config files for new dependency
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/typings.d.ts
+++ web/src/typings.d.ts
@@ -1,8 +1,14 @@
 declare var $: any;
 <<<<<<< HEAD
+<<<<<<< HEAD
 declare var echarts: any;
 declare var CodeMirror: any;
 declare var CBOR: any;
 =======
 declare var echarts: any;
 >>>>>>> b83cdaa... add javascript library
+=======
+declare var echarts: any;
+declare var CodeMirror: any;
+declare var CBOR: any;
+>>>>>>> b69b47d... Update system config files for new dependency
