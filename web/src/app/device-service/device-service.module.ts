60192f301ad617cf848fd2b8f7acc1676b365a43
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-02-21 16:44:34 +0800

    Update Device Service module for other modules import order
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/device-service/device-service.module.ts
+++ web/src/app/device-service/device-service.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 
 import { DeviceServiceRoutingModule } from './device-service-routing.module';
@@ -22,17 +23,33 @@ import { DeviceProfileCenterComponent } from './profile/device-profile-center/de
     FormsModule,
     DeviceServiceRoutingModule,
 =======
+=======
+import { FormsModule } from '@angular/forms';
+>>>>>>> b6dfe8e... Update Device Service module for other modules import order
 
 import { DeviceServiceRoutingModule } from './device-service-routing.module';
 import { DeviceServiceComponent } from './device-service.component';
-
+import { DeviceServiceListComponent } from './device-service-list/device-service-list.component';
+import { DeviceListComponent } from './device/device-list/device-list.component';
+import { EditDeviceComponent } from './device/edit-device/edit-device.component';
+import { AddDeviceComponent } from './device/add-device/add-device.component';
+import { DeviceProfileListComponent } from './profile/device-profile-list/device-profile-list.component';
+import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
+import { AddProfileComponent } from './profile/add-profile/add-profile.component';
+import { DeviceCenterComponent } from './device/device-center/device-center.component';
+import { DeviceProfileCenterComponent } from './profile/device-profile-center/device-profile-center.component';
 
 @NgModule({
-  declarations: [DeviceServiceComponent],
+  declarations: [DeviceServiceComponent, DeviceServiceListComponent, DeviceListComponent, EditDeviceComponent, AddDeviceComponent, DeviceProfileListComponent, EditProfileComponent, AddProfileComponent, DeviceCenterComponent, DeviceProfileCenterComponent],
   imports: [
     CommonModule,
+<<<<<<< HEAD
     DeviceServiceRoutingModule
 >>>>>>> d08a9c7... init scaffold
+=======
+    FormsModule,
+    DeviceServiceRoutingModule,
+>>>>>>> b6dfe8e... Update Device Service module for other modules import order
   ]
 })
 export class DeviceServiceModule { }
