3a269cd750bf649123c931e720beb7ab654601e7
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-02-21 16:37:06 +0800

    Update Profile Resource interface
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/contracts/profile-resource.ts
+++ web/src/app/contracts/profile-resource.ts
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 00ffcb9... Update Profile Resource interface
 import { ResourceOperation } from "./resource-operation";
 
 export interface ProfileResource {
     name: string,
     get: ResourceOperation[],
     set: ResourceOperation[]
+<<<<<<< HEAD
 =======
 export interface ProfileResource {
 >>>>>>> 66d2192... Device and DeviceService program models completed
+=======
+>>>>>>> 00ffcb9... Update Profile Resource interface
 }
