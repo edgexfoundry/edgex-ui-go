f915ef4229b8e0131382dac1c2e6bcccde09a8c0
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-02-22 17:27:33 +0800

    Update msg component for new message formater
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/message/message.component.ts
+++ web/src/app/message/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MessageService } from './message.service';
+import { Message } from './message.service'
 
 @Component({
   selector: 'app-message',
@@ -13,7 +14,11 @@ export class MessageComponent implements OnInit, OnDestroy {
   constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.timer = setInterval(() => { this.messageService.clear() }, 3000)
+    // this.timer = setInterval(() => {this.messageService.clear() }, 3000)
+  }
+
+  close(msg: Message) {
+    this.messageService.messages.splice(this.messageService.messages.indexOf(msg), 1)
   }
 
   ngOnDestroy(): void {
@@ -21,5 +26,4 @@ export class MessageComponent implements OnInit, OnDestroy {
       clearInterval(this.timer);
     }
   }
-
 }
