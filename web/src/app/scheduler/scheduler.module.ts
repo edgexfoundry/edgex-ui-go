d55cf3653b556cd333fe154477f38af306e992f9
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-03-10 21:47:46 +0800

    feat: add router config of scheduler and update scheduler module
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/scheduler/scheduler.module.ts
+++ web/src/app/scheduler/scheduler.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { SchedulerRoutingModule } from './scheduler-routing.module';
 import { SchedulerComponent } from './scheduler.component';
+import { AddIntervalComponent } from './interval/add-interval/add-interval.component';
+import { EditIntervalComponent } from './interval/edit-interval/edit-interval.component';
+import { IntervalListComponent } from './interval/interval-list/interval-list.component';
+import { IntervalActionListComponent } from './intervalaction/interval-action-list/interval-action-list.component';
+import { AddIntervalActionComponent } from './intervalaction/add-interval-action/add-interval-action.component';
+import { EditIntervalActionComponent } from './intervalaction/edit-interval-action/edit-interval-action.component';
+import { IntervalCenterComponent } from './interval/interval-center/interval-center.component';
+import { IntervalActionCenterComponent } from './intervalaction/interval-action-center/interval-action-center.component';
 
 
 @NgModule({
-  declarations: [SchedulerComponent],
+  declarations: [SchedulerComponent, AddIntervalComponent, EditIntervalComponent, IntervalListComponent, IntervalActionListComponent, AddIntervalActionComponent, EditIntervalActionComponent, IntervalCenterComponent, IntervalActionCenterComponent],
   imports: [
     CommonModule,
     SchedulerRoutingModule
