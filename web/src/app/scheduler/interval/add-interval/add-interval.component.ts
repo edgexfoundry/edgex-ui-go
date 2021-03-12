1f4a04f954c3b1194b1108e711617965728a5c16
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-03-12 11:26:29 +0800

    feat: finished the basic feature of add-interval component
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/scheduler/interval/add-interval/add-interval.component.ts
+++ web/src/app/scheduler/interval/add-interval/add-interval.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Interval } from '../../../contracts/v2/interval';
+import { SchedulerService } from '../../../services/scheduler.service';
+import { MultiIntervalResponse } from '../../../contracts/v2/responses/interval-response';
+import { MessageService } from '../../../message/message.service';
 
 @Component({
   selector: 'app-add-interval',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddIntervalComponent implements OnInit {
 
-  constructor() { }
+  interval: Interval = {
+    apiVersion: "v2",
+    id: "",
+    name: "",
+    start: "",
+    end: "",
+    frequency: "",
+    runOnce: false
+  };
+
+  constructor(private schedulerSvc:SchedulerService, 
+    private msgSvc: MessageService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    $(function () {
+      $('[data-toggle="popover"]').popover();
+    })
+
+    $("input[name='intervalStart']").flatpickr({
+      dateFormat: "YmdTHiS",
+      enableTime: true,
+      enableSeconds: true,
+      time_24hr: true,
+      allowInput: false,
+      defaultDate: new Date()
+    });
+    $("input[name='intervalEnd']").flatpickr({
+      dateFormat: "YmdTHiS",
+      enableTime: true,
+      enableSeconds: true,
+      time_24hr: true,
+      allowInput: false,
+      defaultDate: Date.now(),
+    });
   }
 
+  submit() {
+    this.interval.runOnce = this.interval.runOnce?true:false;
+    this.schedulerSvc.addInterval(this.interval).subscribe(() => {
+      this.msgSvc.success("create new interval", `name: ${this.interval.name}`);
+      this.router.navigate(['../interval-list'], { relativeTo: this.route });
+    });
+  }
 }
