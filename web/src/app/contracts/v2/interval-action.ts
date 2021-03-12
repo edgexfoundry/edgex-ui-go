23541e4e95977a1ff6460a1113feedd9f8a98fb0
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-03-12 21:42:06 +0800

    feat: update interval action interface struct
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/contracts/v2/interval-action.ts
+++ web/src/app/contracts/v2/interval-action.ts
@@ -1,10 +1,11 @@
 import { Address } from "./address";
 import { Versionable } from "./common/versionable";
 
-export interface IntervalAction extends Versionable, Address {
+export interface IntervalAction extends Versionable {
     id: string,
     name: string,
     intervalName: string, 
     created: number,
     modified: number
+    address: Address
 }
