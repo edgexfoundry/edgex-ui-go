7b36616ad887cce4361f1b54e15ea2ef34a69e4e
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-03-12 17:30:00 +0800

    feat: finished basic features of edit intervel component
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/scheduler/interval/edit-interval/edit-interval.component.ts
+++ web/src/app/scheduler/interval/edit-interval/edit-interval.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Interval } from '../../../contracts/v2/interval';
+import { SchedulerService } from '../../../services/scheduler.service';
+import { IntervalResponse } from '../../../contracts/v2/responses/interval-response';
+import { MessageService } from '../../../message/message.service';
+import flatpickr from 'flatpickr';
 
 @Component({
   selector: 'app-edit-interval',
   templateUrl: './edit-interval.component.html',
   styleUrls: ['./edit-interval.component.css']
 })
-export class EditIntervalComponent implements OnInit {
+export class EditIntervalComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  interval?: Interval;
+  calendarStart: any;
+  calendarEnd: any;
+
+  constructor(private schedulerSvc:SchedulerService, 
+    private msgSvc: MessageService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(param => {
+      if (param['intervalName']) {
+        this.schedulerSvc.findIntervalByName(param['intervalName']).subscribe((data: IntervalResponse) => {
+          this.interval = data.interval;
+          this.interval.runOnce = this.interval.runOnce?true:false;
+          setTimeout(this.initDatePickr,300);
+        });
+      }
+    });
   }
 
+  initDatePickr() {
+    $('[data-toggle="popover"]').popover({
+      trigger: 'hover'
+    });
+    this.calendarStart = flatpickr("input[name='intervalStart']", {
+      dateFormat: "YmdTHiS",
+      enableTime: true,
+      enableSeconds: true,
+      time_24hr: true,
+      allowInput: false
+    });
+    this.calendarEnd = flatpickr("input[name='intervalEnd']", {
+      dateFormat: "YmdTHiS",
+      enableTime: true,
+      enableSeconds: true,
+      time_24hr: true,
+      allowInput: false
+    });
+  }
+
+  update() {
+    this.schedulerSvc.updateInterval(this.interval as Interval).subscribe(() => {
+      this.msgSvc.success('update interval', `name: ${this.interval?.name}`);
+      this.router.navigate(['../interval-list'],{relativeTo: this.route})
+    });
+  }
+
+  ngOnDestroy(): void {
+    $('[data-toggle="popover"]').popover('dispose');
+  }
 }
+
