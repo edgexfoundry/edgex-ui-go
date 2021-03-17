8daaefb48cd1cbf545d7dea2fca439852656f264
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-03-18 00:26:58 +0800

    feat: Add routes for all components
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/notifications/notifications-routing.module.ts
+++ web/src/app/notifications/notifications-routing.module.ts
@@ -1,11 +1,57 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotificationsComponent } from './notifications.component';
+import { NotificationCenterComponent } from './notification/notification-center/notification-center.component';
+import { NotificationListComponent } from './notification/notification-list/notification-list.component';
+import { SubscriptionCenterComponent } from './subscription/subscription-center/subscription-center.component';
+import { SubscriptionListComponent } from './subscription/subscription-list/subscription-list.component';
+import { AddSubscriptionComponent } from './subscription/add-subscription/add-subscription.component';
+import { EditSubscriptionComponent } from './subscription/edit-subscription/edit-subscription.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NotificationsComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'notification-center',
+        pathMatch: 'full'
+      },
+      {
+        path: 'notification-center',
+        component: NotificationCenterComponent,
+        children: [
+          {
+            path: '',
+            component: NotificationListComponent
+          }
+        ]
+      },
+      {
+        path: 'subscription-center',
+        component: SubscriptionCenterComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'subscription-list',
+            pathMatch: 'full'
+          },
+          {
+            path: 'subscription-list',
+            component: SubscriptionListComponent
+          },
+          {
+            path: 'add-subscription',
+            component: AddSubscriptionComponent
+          },
+          {
+            path: 'edit-subscription',
+            component: EditSubscriptionComponent
+          }
+        ]
+      }
+    ]
   }
 ];
 
