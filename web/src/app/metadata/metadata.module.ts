e6d2fa438f791d69bc57793263f03b4fa7542b8e
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-04-07 12:34:33 +0800

    feat: Add provision watcher module for device auto discover
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/metadata/metadata.module.ts
+++ web/src/app/metadata/metadata.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { MetadataRoutingModule } from './metadata-routing.module';
 import { MetadataComponent } from './metadata.component';
 import { DeviceServiceMediaListComponent } from './device-service/device-service-media-list/device-service-media-list.component';
+import { DeviceServiceListComponent } from './device-service/device-service-list/device-service-list.component';
 import { DeviceListComponent } from './device/device-list/device-list.component';
 import { EditDeviceComponent } from './device/edit-device/edit-device.component';
 import { AddDeviceComponent } from './device/add-device/add-device.component';
@@ -13,9 +14,13 @@ import { EditProfileComponent } from './profile/edit-profile/edit-profile.compon
 import { AddProfileComponent } from './profile/add-profile/add-profile.component';
 import { DeviceCenterComponent } from './device/device-center/device-center.component';
 import { DeviceProfileCenterComponent } from './profile/device-profile-center/device-profile-center.component';
+import { ProvisionWatcherCenterComponent } from './provision-watcher/provision-watcher-center/provision-watcher-center.component';
+import { ProvisionWatcherListComponent } from './provision-watcher/provision-watcher-list/provision-watcher-list.component';
+import { EditProvisionWatcherComponent } from './provision-watcher/edit-provision-watcher/edit-provision-watcher.component';
+import { AddProvisionWatcherComponent } from './provision-watcher/add-provision-watcher/add-provision-watcher.component';
 
 @NgModule({
-  declarations: [MetadataComponent, DeviceServiceMediaListComponent, DeviceListComponent, EditDeviceComponent, AddDeviceComponent, DeviceProfileListComponent, EditProfileComponent, AddProfileComponent, DeviceCenterComponent, DeviceProfileCenterComponent],
+  declarations: [MetadataComponent, DeviceServiceMediaListComponent, DeviceServiceListComponent, DeviceListComponent, EditDeviceComponent, AddDeviceComponent, DeviceProfileListComponent, EditProfileComponent, AddProfileComponent, DeviceCenterComponent, DeviceProfileCenterComponent, ProvisionWatcherCenterComponent, ProvisionWatcherListComponent, EditProvisionWatcherComponent, AddProvisionWatcherComponent],
   imports: [
     CommonModule,
     FormsModule,
