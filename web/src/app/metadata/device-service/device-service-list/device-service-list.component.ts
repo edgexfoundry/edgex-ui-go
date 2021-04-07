c9a660a23944df1ce7f7e1244b30623f431a6ed2
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-04-07 12:35:14 +0800

    refactor: update metadata module to match edgex v2
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/metadata/device-service/device-service-list/device-service-list.component.ts
+++ web/src/app/metadata/device-service/device-service-list/device-service-list.component.ts
@@ -1,4 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+/*******************************************************************************
+ * Copyright © 2021-2022 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author: Huaqiao Zhang, <huaqiaoz@vmware.com>
+ *******************************************************************************/
+
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { DeviceService } from '../../../contracts/v2/device-service';
+import { MetadataService } from '../../../services/metadata.service';
+import { MultiDeviceServiceResponse } from '../../../contracts/v2/responses/device-service-response';
 
 @Component({
   selector: 'app-device-service-list',
@@ -7,9 +27,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceServiceListComponent implements OnInit {
 
-  constructor() { }
+  @Output() singleDeviceSvcSelectedEvent = new EventEmitter<DeviceService>();
+  deviceServiceList: DeviceService[] = [];
+  @Input() deviceSvcSelected?: DeviceService;
+  pagination: number = 1;
+  pageLimit: number = 5;
+  pageOffset: number = (this.pagination - 1) * this.pageLimit;
+
+  constructor(private metaSvc: MetadataService) { }
 
   ngOnInit(): void {
+    this.findAllDeviceSvcPagination();
   }
 
+  findAllDeviceSvcPagination() {
+    this.metaSvc
+    .findAllDeviceServicesPagination(this.pageOffset, this.pageLimit)
+    .subscribe((resp: MultiDeviceServiceResponse) => {
+      this.deviceServiceList = resp.services;
+    })
+  }
+
+  onSingleDeviceSvcSelectedEmitter() {
+    this.singleDeviceSvcSelectedEvent.emit(this.deviceSvcSelected);
+  }
+
+  isSvcChecked(name: string): boolean {
+    return this.deviceSvcSelected?.name === name
+  }
+
+  selectOneSvc(event: any, name: string) {
+    const checkbox = event.target;
+    let self = this;
+    if (checkbox.checked) {
+      this.deviceServiceList.forEach(function (svc) {
+        if (svc.name === name) {
+          self.deviceSvcSelected = svc;
+        }
+      });
+    } else {
+      this.deviceSvcSelected = undefined;
+    }
+    this.onSingleDeviceSvcSelectedEmitter();
+  }
+
+  prePage() {
+    this.setPagination(-1);
+    this.findAllDeviceSvcPagination();
+  }
+
+  nextPage() {
+    this.setPagination(1);
+    this.findAllDeviceSvcPagination();
+  }
+
+  setPagination(n?: number) {
+    if (n === 1) {
+      this.pagination += 1;
+    } else {
+      this.pagination -= 1;
+    }
+    this.pageOffset = (this.pagination - 1) * this.pageLimit;
+  }
+
+  resetPagination() {
+    this.pagination = 1;
+  }
 }
