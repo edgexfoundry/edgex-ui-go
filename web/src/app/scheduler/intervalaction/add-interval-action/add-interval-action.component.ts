595f9b5bdb12708e82b273c281690c642b32cc30
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-04-12 16:33:53 +0800

    feat: Finish basic feature about how to add a new interval action
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/scheduler/intervalaction/add-interval-action/add-interval-action.component.ts
+++ web/src/app/scheduler/intervalaction/add-interval-action/add-interval-action.component.ts
@@ -1,4 +1,29 @@
+/*******************************************************************************
+ * Copyright © 2021-2022 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author: Huaqiao Zhang, <huaqiaoz@vmware.com>
+ *******************************************************************************/
+
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { SchedulerService } from '../../../services/scheduler.service';
+import { MessageService } from '../../../message/message.service';
+import { ErrorService } from '../../../services/error.service';
+import { Address } from '../../../contracts/v2/address';
+
+import { IntervalAction } from '../../../contracts/v2/interval-action';
+import { Interval } from '../../../contracts/v2/interval';
 
 @Component({
   selector: 'app-add-interval-action',
@@ -7,9 +32,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddIntervalActionComponent implements OnInit {
 
-  constructor() { }
+  intervalAction: IntervalAction;
+  addressEmailRecipients: string = "";
+
+  constructor(private schedulerSvc:SchedulerService, 
+    private msgSvc: MessageService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private errSvc: ErrorService) { 
+      this.intervalAction = {} as IntervalAction;
+      this.intervalAction.address = {} as Address;
+      this.intervalAction.address.type = 'REST';
+      this.intervalAction.address.httpMethod = 'GET';
+      this.intervalAction.address.retained = false;
+      this.intervalAction.address.autoReconnect = true;
+    }
 
   ngOnInit(): void {
+    $('[data-toggle="popover"]').popover({
+      trigger: 'hover'
+    });
   }
 
+  typeToggle(type: string) {
+    this.intervalAction.address.type = type;
+  }
+
+  onSingleIntervalSelected(interval: Interval) {
+    this.intervalAction.intervalName = interval.name;
+  }
+
+  isPureIntegerType(value: any): boolean {
+    if (!isNaN(value) && (parseFloat(value) === parseInt(value))) {
+      return true
+    }
+    return false
+  }
+
+  validate(): boolean {
+    let result = true;
+    let basic =  this.intervalAction.name && this.intervalAction.intervalName; 
+    switch (this.intervalAction.address.type) {
+      case 'REST':
+        if (basic && this.intervalAction.address.host && this.isPureIntegerType(this.intervalAction.address.port) &&
+          this.intervalAction.address.port) {
+            result = false
+        }
+        break
+      case 'MQTT': 
+        if (basic && this.intervalAction.address.host && this.isPureIntegerType(this.intervalAction.address.port) &&
+          this.intervalAction.address.port &&
+          this.intervalAction.address.publisher &&
+          this.intervalAction.address.topic) {
+            result = false
+        }
+        break
+      case 'EMAIL':
+        if (basic) {
+          result = false
+        }
+    }
+    return result
+  }
+
+  submit() {
+    this.intervalAction.address.recipients = this.addressEmailRecipients.split(',');
+    console.log(this.intervalAction.address.recipients)
+    this.schedulerSvc.addIntervalAction(this.intervalAction).subscribe(() => {
+      this.msgSvc.success('Add interval action',`name: ${this.intervalAction.name}`)
+    })
+  }
 }
