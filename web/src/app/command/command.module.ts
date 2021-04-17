5d2e707b0ffa8eb90bf569759b8969dcdc0a24ab
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-04-17 23:28:04 +0800

    feat: add new component export
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/command/command.module.ts
+++ web/src/app/command/command.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { CommandRoutingModule } from './command-routing.module';
 import { DeviceCoreCommandComboListComponent } from './device-core-command/device-core-command-combo-list/device-core-command-combo-list.component';
 import { DeviceAssociatedCoreCommandListComponent } from './device-core-command/device-associated-core-command-list/device-associated-core-command-list.component';
+import { DeviceCoreCommandListComponent } from './device-core-command/device-core-command-list/device-core-command-list.component';
 
 
 @NgModule({
-  declarations: [DeviceCoreCommandComboListComponent, DeviceAssociatedCoreCommandListComponent],
+  declarations: [DeviceCoreCommandComboListComponent, DeviceAssociatedCoreCommandListComponent, DeviceCoreCommandListComponent],
   imports: [
     CommonModule,
+    FormsModule,
     CommandRoutingModule
+  ],
+  exports: [
+    DeviceCoreCommandComboListComponent, DeviceAssociatedCoreCommandListComponent, DeviceCoreCommandListComponent
   ]
 })
 export class CommandModule { }
