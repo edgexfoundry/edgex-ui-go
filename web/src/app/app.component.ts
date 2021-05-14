fe4fd0ca06443b819d6b9a788f0f0b22fa7b2e00
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-05-14 09:56:25 +0800

    feat: navigation chain is finished on header bar on page
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -16,17 +16,16 @@ export class AppComponent implements OnInit {
   shrinkSidebarOnly: boolean = false;
   shrinkCenterNo: boolean = false;
 
-
   navEnd: Observable<NavigationEnd>;
   navStart: Observable<NavigationStart>;
   navRecognized: Observable<RoutesRecognized>;
-
+  
   childStart: Observable<ChildActivationStart>;
 
   navChainMap = new Map<string, string>();
   navChainCache: string[] = [];
   navChain: string[] = [];
-  currentNav?: string;
+  currentNav: string = '';
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     this.navRecognized = router.events.pipe(
@@ -47,47 +46,23 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    // console.log(this.router.config);
-    // this.activatedRoute.root.queryParams
-    //   .subscribe(queryParams => console.log('The URL changed to: ' + queryParams));
-    // this.activatedRoute.url
-    //   .subscribe(url => console.log('The URL changed to: ' + url));
-    // this.navStart.subscribe(evt => console.log(evt));
-    // this.navRecognized.subscribe(evt => console.log(evt.state.root.pathFromRoot));
-
-    // this.childStart.subscribe(evt => console.log(evt));
-
-
     this.navEnd.subscribe(evt => {
       this.navChainCache = [];
       this.navChainMap = new Map<string, string>();
-      // console.log(evt);
 
       this.currentNav = evt.urlAfterRedirects.split('?')[0].split('/').pop() as string;
-      // this.navChainCache.set(last, evt.urlAfterRedirects);
       this.navChain = evt.urlAfterRedirects.split('?')[0].split('/');
       this.navChain.shift();
-      // this.currentNav = this.navChain.slice(0, 1)[0];
-      // console.log(this.navChain);
       let self = this;
       let reverseNav1 = this.navChain.map(x => x).reverse();
-      // console.log(this.navChain);
-      // console.log(reverseNav1)
       reverseNav1.forEach(function (item, index) {
-        // console.log(item, index)
         let reverseNav2 = self.navChain.map(x => x).reverse();
         let t = reverseNav2.slice(index)
-        // console.log(t)
-        // console.log(t.reverse().push(item))
         let nav = t.reverse().join('/');
-        // console.log(nav)
         self.navChainCache.push("/" + nav)
         self.navChainMap.set(item, "/" + nav)
       });
       this.navChainCache.reverse();
-      // console.log(this.navChainCache)
-      // console.log(this.navChainMap)
-      // console.log(this.navChain)
     });
 
     $(window).resize(() => {
@@ -98,6 +73,14 @@ export class AppComponent implements OnInit {
         this.shrink = false;
       }
     });
+  }
+
+  pathFormat(path: string): string {
+    let pathArr = path.split('-');
+    pathArr.forEach((e,i) =>{
+      pathArr[i] = e.charAt(0).toUpperCase() + e.substring(1);
+    });
+    return pathArr.join(' ');
   }
 
   sideBarToggle() {
