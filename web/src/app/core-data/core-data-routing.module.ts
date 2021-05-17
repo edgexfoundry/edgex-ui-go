016f4c792c70aed80bc8237f0c687fdc2bb9011b
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-05-17 11:46:34 +0800

    feat: Finished basic features of core data module
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/core-data/core-data-routing.module.ts
+++ web/src/app/core-data/core-data-routing.module.ts
@@ -1,11 +1,45 @@
+/*******************************************************************************
+ * Copyright © 2021-2022 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author: Huaqiao Zhang, <huaqiaoz@vmware.com>
+ *******************************************************************************/
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { CoreDataComponent } from './core-data.component';
+import { EventComponent } from './event/event.component';
+import { ReadingComponent } from './reading/reading.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CoreDataComponent
+    component: CoreDataComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'event',
+        pathMatch: 'full'
+      },
+      {
+        path: 'event',
+        component: EventComponent
+      },
+      {
+        path: 'reading',
+        component:  ReadingComponent
+      }
+    ]
   }
 ];
 
