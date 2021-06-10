e0ceb68d16a118c2389d05337f09aaffedb00859
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-06-10 15:28:20 +0800

    feat: add new base reponse interface to match edgex v2 updates
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/contracts/v2/common/base-response.ts
+++ web/src/app/contracts/v2/common/base-response.ts
@@ -9,3 +9,20 @@ export interface BaseResponse extends Versionable {
 export interface BaseWithIdResponse extends BaseResponse {
     id: string
 }
+
+// Now only be used for system agent health check
+export interface BaseWithServiceNameResponse extends BaseResponse {
+    serviceName: string
+}
+
+// Now only be used for system agent metric check with docker executer 
+export interface BaseWithMetricsResponse extends BaseResponse {
+    serviceName: string,
+    metrics: any
+}
+
+// Now only be used for system agent config check 
+export interface BaseWithConfigResponse extends BaseResponse {
+    serviceName: string,
+    config: any
+}
