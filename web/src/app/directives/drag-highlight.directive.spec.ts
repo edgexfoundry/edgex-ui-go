ad1decb4bd72f1447ea6f8c84f0c196c73f7f56a
Huaqiao Zhang <huaqiaoz@vmware.com>
2021-08-04 12:30:43 +0800

    feat: add drag-drop zone highlight directives to improve user experience when users use drag-drop function to select pipeline functions
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

---
+++ web/src/app/directives/drag-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DragHighlightDirective } from './drag-highlight.directive';
+
+describe('DragHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DragHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
