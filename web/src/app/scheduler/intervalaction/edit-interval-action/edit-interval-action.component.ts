d88a2a82eaf5af1a13a3b4c48d6cbd2ae348549a
badboy-huaqiao <huaqiaoz@vmware.com>
2022-02-16 19:52:09 +0800

    fix: issue-#494 about scheduler updates and bug fixed (#496)
    * fix: issue-#494 about scheduler updates and bug fixed
    
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>
    
    * fix: remove debugg code
    
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- web/src/app/scheduler/intervalaction/edit-interval-action/edit-interval-action.component.ts
+++ web/src/app/scheduler/intervalaction/edit-interval-action/edit-interval-action.component.ts
@@ -27,7 +27,7 @@ import { Interval } from '../../../contracts/v2/interval';
 import { IntervalActionResponse } from '../../../contracts/v2/responses/interval-action-response';
 import { IntervalResponse } from '../../../contracts/v2/responses/interval-response';
 import { CoreCommand } from '../../../contracts/v2/core-command';
-
+import { CoreCommandParameter } from '../../../contracts/v2/core-command';
 import flatpickr from 'flatpickr';
 
 @Component({
@@ -51,9 +51,13 @@ export class EditIntervalActionComponent implements OnInit {
   selectedIntervalDefault?: Interval;
   templateSelected: string = "custom";
   coredataRequestParameter = '';
+  commandServiceTemplateRequestParameters: CoreCommandParameter[] = [];
+  pushEventOfGetCmdParamter = "yes";
+  returnEventOfGetCmdParamter = "yes";
   calendarStart: any;
 
   coredataSvcAvailableAPI: string[] = [
+    //one of delete or data clean APIs of core data service 
     "/api/v2/event/age/"
   ];
 
@@ -77,7 +81,7 @@ export class EditIntervalActionComponent implements OnInit {
 
           this.intervalActionOrigin.adminState = this.intervalActionOrigin.adminState === '' ? "UNLOCKED" : this.intervalActionOrigin.adminState;
           this.intervalAction.adminState = this.intervalAction.adminState === '' ? "UNLOCKED" : this.intervalAction.adminState;
-
+          
           this.addressEmailRecipients  = this.intervalAction.address.recipients?this.intervalAction.address.recipients.toString():'';
           this.findDefaultSelectedIntervalByName(this.intervalAction.intervalName);
           setTimeout(() => {
@@ -173,10 +177,14 @@ export class EditIntervalActionComponent implements OnInit {
 
   onCmdMethodSelected(method: string) {
     this.intervalAction.address.httpMethod = method;
+    setTimeout(() => {
+      this.renderPopoverComponent();
+    }, 500);
   }
 
   onCommandSelected(cmd: CoreCommand) {
     this.intervalAction.address.path = cmd.path;
+    this.commandServiceTemplateRequestParameters = cmd.parameters;
   }
 
   onSingleIntervalSelected(interval: Interval) {
@@ -195,14 +203,17 @@ export class EditIntervalActionComponent implements OnInit {
     let basic =  this.intervalAction.name && this.intervalAction.intervalName; 
     switch (this.intervalAction.address.type) {
       case this.addr_type_REST:
-        if (basic && this.intervalAction.address.host && this.isPureIntegerType(this.intervalAction.address.port) &&
-          this.intervalAction.address.path && this.intervalAction.address.httpMethod) {
+        if (basic && this.intervalAction.address.host && 
+          this.isPureIntegerType(this.intervalAction.address.port) &&
+          this.intervalAction.address.path && 
+          this.intervalAction.address.httpMethod) {
             result = false
         }
         break
       case this.addr_type_MQTT: 
-        if (basic && this.intervalAction.address.host && this.isPureIntegerType(this.intervalAction.address.port) &&
-          this.intervalAction.address.port &&
+        if (basic && this.intervalAction.address.host && 
+          this.isPureIntegerType(this.intervalAction.address.port) &&
+          // this.intervalAction.address.port && // if the value of port is 0 will not be passed
           this.intervalAction.address.publisher &&
           this.intervalAction.address.topic) {
             result = false
@@ -216,9 +227,62 @@ export class EditIntervalActionComponent implements OnInit {
     return result
   }
 
+  getAllCmdTemplateParametersValue(): string {
+    if (this.intervalAction.address.type !== this.addr_type_REST ||
+      this.templateSelected !== this.template_type_command) {
+      return '';
+    }
+    if (this.intervalAction.address.httpMethod == 'GET') return '';
+    let params: any = {};
+    this.commandServiceTemplateRequestParameters?.forEach(p => {
+      if ($(`#cmd-param-${p.resourceName}`).val().trim() !== "") {
+        params[p.resourceName] = $(`#cmd-param-${p.resourceName}`).val().trim();
+      }
+    });
+    return JSON.stringify(params)
+  }
+
+  resolveCommandTemplateParameterSuffix() {
+    //path value example: /api/v2/device/name/Random-Integer-Device/Int16Array?ds-pushevent=no&ds-returnevent=yes 
+    if (this.intervalAction.address.path.indexOf('ds-pushevent') !== -1 ||  
+        this.intervalAction.address.path.indexOf('ds-returnevent') !== -1) {
+        let path = this.intervalAction.address.path.split('?');
+        this.intervalAction.address.path = path[0];
+        let parametersStr = path[1];
+        let parameters = parametersStr.split('&');
+        parameters.forEach((paramStr) => {
+          let paramKV = paramStr.split('=');
+          if (paramKV[0] === 'ds-pushevent') {
+            this.pushEventOfGetCmdParamter = paramKV[1]
+          } else if (paramKV[0] === 'ds-returnevent') {
+            this.returnEventOfGetCmdParamter = paramKV[1]
+          }
+        })
+
+    }
+  }
+
+  resetPathParameterSuffix() {
+    if (this.intervalAction.address.path.indexOf('ds-pushevent') !== -1 ||  
+        this.intervalAction.address.path.indexOf('ds-returnevent') !== -1) {
+        this.intervalAction.address.path = this.intervalAction.address.path.split('?')[0];
+    }
+    this.intervalAction.address.path = `${this.intervalAction.address.path}?ds-pushevent=${this.pushEventOfGetCmdParamter}&ds-returnevent=${this.returnEventOfGetCmdParamter}`;
+  }
+
   save() {
     this.intervalAction.address.recipients = this.addressEmailRecipients.split(',');
     this.intervalAction.address.port = Number(this.intervalAction.address.port);
+    
+    if (this.intervalAction.address.type === this.addr_type_REST &&
+      this.templateSelected === this.template_type_command) {
+        if (this.intervalAction.address.httpMethod === 'GET') {
+          this.resetPathParameterSuffix()
+        } else if (this.intervalAction.address.httpMethod === 'PUT') {
+          this.intervalAction.content = this.getAllCmdTemplateParametersValue();
+        }
+    }
+    
     this.schedulerSvc.updateIntervalAction(this.intervalAction).subscribe(()=>{
       this.msgSvc.success('Update interval action',`name: ${this.intervalAction.name}`);
       this.router.navigate(['../interval-action-list'],{ relativeTo: this.route });
