b529499ada60d8f3399282414b00f575858c0428
Anthony Casagrande <anthony.j.casagrande@intel.com>
2022-07-06 16:21:31 -0700

    fix: reset operationStatus when http call returns
    - refactor and cleanup of rule start/stop/restart
    
    Signed-off-by: Anthony Casagrande <anthony.j.casagrande@intel.com>

--- web/src/app/services/rule-engine.service.ts
+++ web/src/app/services/rule-engine.service.ts
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright © 2022-2023 VMware, Inc. All Rights Reserved.
+ * Copyright © 2022 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -10,7 +11,7 @@
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * @author: Huaqiao Zhang, <huaqiaoz@vmware.com>
  *******************************************************************************/
 
@@ -159,27 +160,11 @@ export class RuleEngineService {
     )
   }
 
-  startRule(id: string): Observable<string> {
-    let url = `${this.ruleUrl}/${id}/start`;
+  executeRuleCommand(id: string, command: string) {
+    let url = `${this.ruleUrl}/${id}/${command}`;
     return this.http.post(url,null,{responseType: 'text'})
-    .pipe(
-      catchError(error => this.errorSvc.handleError(error))
-    )
-  }
-
-  stopRule(id: string): Observable<string> {
-    let url = `${this.ruleUrl}/${id}/stop`;
-    return this.http.post(url,null,{responseType: 'text'})
-    .pipe(
-      catchError(error => this.errorSvc.handleError(error))
-    )
-  }
-
-  restartRule(id: string): Observable<string> {
-    let url = `${this.ruleUrl}/${id}/restart`;
-    return this.http.post(url,null,{responseType: 'text'})
-    .pipe(
-      catchError(error => this.errorSvc.handleError(error))
-    )
+      .pipe(
+        catchError(error => this.errorSvc.handleError(error))
+      )
   }
 }
