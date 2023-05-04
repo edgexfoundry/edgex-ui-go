5f229313553dc91b9ee90f1a6a748fc527e9160b
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-05-04 16:37:18 +0200

    refactor(snap): Switch to yaml config, upgrade go (#602)
    Signed-off-by: Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>

--- snap/local/bin/edgex-ui-wrapper.sh
+++ snap/local/bin/edgex-ui-wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
-cd "$SNAP"
-exec ./bin/edgex-ui-server --confdir "$SNAP_DATA/config/edgex-ui-server/res"
+# Change to $SNAP directory because the application loads static files relative
+#   to the working directory
+cd $SNAP
+exec ./bin/edgex-ui-server --configDir $SNAP_DATA/config/edgex-ui-server/res
