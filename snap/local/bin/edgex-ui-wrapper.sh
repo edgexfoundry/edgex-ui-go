6eeb2ce6fc2afc5ffacdf29fd0663685f6ab0964
Mengyi Wang <mengyi.wang@canonical.com>
2023-10-24 18:04:11 +0200

    feat!: Remove snap packaging
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/bin/edgex-ui-wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Change to $SNAP directory because the application loads static files relative
-#   to the working directory
-cd $SNAP
-exec ./bin/edgex-ui-server --configDir $SNAP_DATA/config/edgex-ui-server/res
